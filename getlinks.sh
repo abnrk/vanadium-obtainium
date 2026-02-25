5125dbbbb32918416831bee746b7c089a579d8bc
abnrk <adudowas@gmail.com>
2026-02-25 01:32:23 +0000

    Update getlinks.sh

--- getlinks.sh
+++ getlinks.sh
@@ -1,3 +1,3 @@
-LAST_CHANGE=$(curl "https://storage.googleapis.com/chromium-browser-snapshots/AndroidDesktop_arm64/LAST_CHANGE")
-ZIP_URL="https://storage.googleapis.com/chromium-browser-snapshots/AndroidDesktop_arm64/$LAST_CHANGE/chrome-android-desktop.zip"
-echo $ZIP_URL > links.txt
+VERSION=$(curl -s https://api.github.com/repos/GrapheneOS/apps.grapheneos.org/contents/apps/packages/app.vanadium.browser | grep '"name": "[0-9]*"' | tail -n 1 | sed -r 's/[^0-9]//g' | cut -c 1-7)33
+echo "https://apps.grapheneos.org/packages/app.vanadium.browser/$VERSION/base.apk" > links.txt
+echo "https://apps.grapheneos.org/packages/app.vanadium.trichromelibrary/$VERSION/base.apk" >> links.txt
